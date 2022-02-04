e406a8e65d3973ba408fbd58eea56247f0a8957b
IlirImeri <ilirimeri2002@gmail.com>
2022-02-05 00:05:01 +0100

    changed dashboard style

--- php/readData.php
+++ php/readData.php
@@ -1,73 +1,73 @@
 <?php
 session_start();
 ?>
-
-<style>
-table, td, th {
-  border: 1px solid #ddd;
-  text-align: left;
-}
-
-table {
-  border-collapse: collapse;
-}
-
-th, td {
-  padding: 15px;
-}
-</style>
-</head>
-<body>
-
 <!DOCTYPE html>
-<html>
-<head>
-  <?php
-  if (isset($_SESSION["ID"])) {
-    echo "<form action='logout.php'><input name='submit' type='submit' value='LOGOUT'/></form>";
-    echo "<form action='dash-functions.php' method='post'>
-            <button type='submit' name='submit'>ADD</button>
-          </form>";
-    echo "<form action='logout.php'><input type='submit' value='DELETE'/></form>";
-    echo "<form action='logout.php'><input type='submit' value='UPDATE'/></form>";
-    echo "<form action='logout.php'><input type='submit' value='SEARCH'/></form>";
+<html lang="en" dir="ltr">
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="./../css/dashboard-style.css">
+		<title></title>
+	</head>
+	<body>
 
-    require_once('db-connect.php');
+	</body>
+</html>
 
-    $sql = "SELECT ID, username, email, URL, password FROM dashboard WHERE owner = ?;";
+<?php
+if (isset($_SESSION["ID"])) {
+  echo "
+		<form action='logout.php'><input name='submit' type='submit' value='LOGOUT' class='button'/></form>
+		<div class='container'>
+			<div class='button-space'>
+	  		<form action='dash-functions.php' method='post'><button type='submit' name='submit' class='button'>ADD</button></form>
+	  		<form action='logout.php'><input type='submit' value='DELETE' class='button'/></form>
+	   		<form action='logout.php'><input type='submit' value='UPDATE' class='button'/></form>
+	   		<form action='logout.php'><input type='submit' value='SEARCH' class='button'/></form>
+			</div>
+	";
 
-    if ($statement = mysqli_prepare($conn, $sql)) {
-      mysqli_stmt_bind_param($statement, 's', $_SESSION["ID"]);
-      mysqli_stmt_execute($statement);
-      $result = mysqli_stmt_get_result($statement);
-      if ($result->num_rows>0) {
+  require_once('db-connect.php');
+
+  $sql = "SELECT ID, username, email, URL, password FROM dashboard WHERE owner = ?;";
+
+  if ($statement = mysqli_prepare($conn, $sql)) {
+    mysqli_stmt_bind_param($statement, 's', $_SESSION["ID"]);
+    mysqli_stmt_execute($statement);
+    $result = mysqli_stmt_get_result($statement);
+    if ($result->num_rows>0) {
+      echo
+      "<table>
+          <thead>
+            <tr class='head-table'>
+              <th>ID</th>
+              <th>Username</th>
+              <th>Email</th>
+              <th>URL</th>
+              <th>Password</th>
+            </tr>
+          </thead>";
+      while ($row = mysqli_fetch_assoc($result)) {
         echo
-        "<table>
-          <tr>
-            <th>ID</th>
-            <th>Username</th>
-            <th>Email</th>
-            <th>URL</th>
-            <th>Password</th>
-          </tr>";
-        while ($row = mysqli_fetch_assoc($result)) {
-          echo
-          "<tr>
-            <td>".$row["ID"]."</td>
-            <td>".$row["username"]."</td>
-            <td>".$row["email"]."</td>
-            <td>".$row["URL"]."</td>
-            <td>".$row["password"]."</td>
-          </tr>";
-        }
-        echo "</table>";
-      }else {
-        echo "0 results";
+        "<tbody>
+          <tr class='tr'>
+            <td class='td'>".$row["ID"]."</td>
+            <td class='td'>".$row["username"]."</td>
+            <td class='td'>".$row["email"]."</td>
+            <td class='td'>".$row["URL"]."</td>
+            <td class='td'>".$row["password"]."</td>
+          </tr>
+        </tbody>";
       }
-      mysqli_stmt_close($statement);
+      echo
+       "</table>
+      </div>";
+    }else {
+      echo "0 results";
     }
-  }else {
-    header("Location: ./../main/login.php");
-    exit();
+    mysqli_stmt_close($statement);
   }
-  ?>
+}else {
+  header("Location: ./../main/login.php");
+  exit();
+}
+?>
